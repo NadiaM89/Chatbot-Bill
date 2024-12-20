f6802a8e20a7fc6252a4993a5c89a80215a37917
NadiaM89 <nmasakrinec@gmail.com>
2024-12-20 12:14:54 -0300

    comentario_routerspy

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 from fastapi import FastAPI
 from routers import router
 
+# Crear una instancia de la aplicación FastAPI
 app = FastAPI()
 
+# Establecer el título de la aplicación
 app.title = 'BILL'
 
+# Incluir el router definido en el módulo routers
 app.include_router(router)
--- routers.py
+++ routers.py
@@ -2,15 +2,28 @@ from fastapi import APIRouter, HTTPException, status
 from models import AskInput, AskOutput
 from services import chatbot
 
+# Crear una instancia del router de FastAPI
 router = APIRouter()
 
 @router.post("/ask/", tags=['Consulta'], status_code=status.HTTP_200_OK, response_model=AskOutput)
 async def ask_question(ask: AskInput):
-    """Endpoint para realizar una pregunta."""
+    """
+    Endpoint para realizar una pregunta.
+
+    Args:
+    ask (AskInput): Objeto que contiene la pregunta del usuario.
+
+    Returns:
+    AskOutput: Objeto que contiene la respuesta generada por el chatbot.
+    """
     try:
+        # Generar la respuesta utilizando la función chatbot
         respuesta = chatbot(ask.question)
+        
+        # Devolver la respuesta en el formato especificado por AskOutput
         return AskOutput(answer=respuesta)
     except Exception as e:
+        # Manejar cualquier excepción que ocurra y devolver un error HTTP 500
         raise HTTPException(status_code=500, detail=str(e))
 
 
