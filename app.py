c2bdbe52dc78a2d90bc39c69748c05782fca1409
NadiaM89 <nmasakrinec@gmail.com>
2025-03-28 21:51:01 -0300

    Versión 2 de Bill: sin análisis de sentimiento y rerank ajustado

--- app.py
+++ app.py
@@ -1,12 +1,16 @@
 import streamlit as st
 import requests
 
-
 # Título con emoji de lentes al lado del nombre "BILL"
 st.title("BILL 🤓")
 
-# Entrada del usuario
-user_input = st.text_input("¡Hazme una pregunta!:")
+# Introducción y datos personales con tamaño más pequeño
+st.markdown('<h6>Creado por Nadia Maskarinec 🧠</h6>', unsafe_allow_html=True)
+
+
+# Entrada del usuario con tamaño más grande
+st.markdown('<h1>¡Hazme una pregunta!</h1>', unsafe_allow_html=True)
+user_input = st.text_input("", key="user_input")
 
 if st.button("Enviar"):
     # Realizar la solicitud a la API de FastAPI
@@ -17,3 +21,8 @@ if st.button("Enviar"):
         st.write(f"{answer}")
     else:
         st.write("Error al obtener la respuesta. Inténtalo de nuevo.")
+
+# Pie de página con tamaño más pequeño
+st.markdown('<p><a href="https://www.linkedin.com/in/nadia-maskarinec/">🔗 Mi LinkedIn </a></p>', unsafe_allow_html=True)
+st.markdown('<h6>📍 Creado por Nadia Maskarinec - Córdoba, Argentina.</h6>', unsafe_allow_html=True)
+
